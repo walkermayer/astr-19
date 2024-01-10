8c21b5ea8b793d2e5c54e15467a2603740f95410
walkermayer <156133455+walkermayer@users.noreply.github.com>
2024-01-09 16:40:41 -0800

    Add files via upload

---
+++ prmt1jrnl1.py
@@ -0,0 +1,11 @@
+#first function for printing name and pronouns
+def PrintNameNInfo(): 
+	print(f'Hello my name is Walker Mayer, and my preferred prounouns are he/him.'
+		f' My favorite movie is Cars, because I just love a good animated film.'
+		f' My favorite food would probably be breakfast food, especially Belgian waffles.' )
+
+def main():
+	PrintNameNInfo()
+
+if __name__ == "__main__":
+	main()
